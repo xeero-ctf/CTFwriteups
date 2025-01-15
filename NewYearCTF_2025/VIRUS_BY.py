46bdb44ab9c9d1106481e850438aa43fdb894ff9
anonymus124 <morandigabriele279@gmail.com>
2025-01-15 21:24:33 +0100

    Add files via upload

---
+++ NewYearCTF_2025/VIRUS_BY.py
@@ -0,0 +1,34 @@
+from pwn import *
+
+
+for j in range(5):
+    for i in range(5):
+        r= remote("ctf.mf.grsu.by", 9033)
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"neshta")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"something")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"2005")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"Delphi")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"Belarus")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"Dziadulja Apanas")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"Alivaria")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"svchost.com")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(b"Lukashenko")
+        print(r.recvuntil(b">").decode("utf-8"))
+        r.sendline(f"{i}:{j}")
+        ciao=r.recvall().decode("utf-8")
+        if "grodno" in ciao:
+            print("ciao", ciao)
+            break
+        r.close()
+
+#grodno{db9ee02312fe3ba5890a8f1cb9}
+
